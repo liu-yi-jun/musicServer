c3891a1449a647fe673fdd02064d797f84fcbb7c
刘奕俊 <eigene@163.com>
2020-12-13 09:56:58 +0800

    调节乐谱高度

--- routes/tap.js
+++ routes/tap.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 var db = require('../db/db');
@@ -7,13 +8,15 @@ let util = require('../util/util')
 router.get('/getRandomTap', (req, res, next) => {
     let limit = req.query.limit
     let sql = `SELECT id, tapTitle, author FROM tap ORDER BY RAND() limit ${limit}`
-    db.coreQuery(sql).then(result => res.json(util.success(result))).catch(err => next(err))
+    db.coreQuery(sql).then(result =>{
+        res.json(util.success(result))
+    }).catch(err => next(err))
 })
 
 router.get('/searchTap', async (req, res, next) => {
     try {
-        let { tapTitle, pageSize, pageIndex } = req.query
-        let sql = `SELECT * FROM tap where tapTitle like '%${tapTitle}%' LIMIT ${pageSize} OFFSET ${pageSize * (pageIndex - 1)}`
+        let { tapTitle, limit } = req.query
+        let sql = `SELECT * FROM tap where tapTitle like '%${tapTitle}%' LIMIT ${limit} `
         let taps = await db.coreQuery(sql)
         res.json(util.success(taps))
     } catch (err) {
@@ -24,6 +27,8 @@ router.get('/getTapDetail', async (req, res, next) => {
     try {
         let { tapId, pageSize, pageIndex,userId } = req.query
         let tap = await db.onlyQuery('tap', 'id', tapId)
+        
+        tap[0].tapPicLink = JSON.parse(tap[0].tapPicLink) 
         let tapRecord = await db.paging('taprecord', pageSize, pageIndex, { tapId }, ['likes DESC'])
         tapRecord = await db.isLike(tapRecord, 'taprecordlike', userId, 'taprecordId')
         res.json(util.success({
